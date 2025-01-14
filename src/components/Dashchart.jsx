239982ebf4139ea0db6f67086cb5c2beb8f5ac41
Vulpes231 <whykayshot@gmail.com>
2025-01-14 20:19:34 +0100

    trading

--- src/components/Dashchart.jsx
+++ src/components/Dashchart.jsx
@@ -2,14 +2,12 @@
 import React from "react";
 import { styles } from "../constants/styles";
 import Dashwallet from "./Dashwallet";
-import Netgrowth from "./Netgrowth";
 import BitcoinChart from "./Bitcoinchart";
 
 const Dashchart = () => {
   return (
     <div className="grid md:grid-cols-3 gap-6">
       <BitcoinChart />
-      {/* <Netgrowth /> */}
       <Dashwallet />
     </div>
   );
