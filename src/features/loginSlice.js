0ff1b26f06131ad5fa3c73fe81ec055fb8198f1c
Vulpes231 <whykayshot@gmail.com>
2025-09-03 01:28:12 +0100

    user fix

--- src/features/loginSlice.js
+++ src/features/loginSlice.js
@@ -4,72 +4,72 @@ import axios from "axios";
 import { devServer, liveServer, sendError } from "../constants/constant";
 
 const initialState = {
-  loginLoading: false,
-  loginError: false,
-  accessToken: false,
-  country: false,
-  isProfileComplete: false,
-  email: false,
-  otpCode: false,
+	loginLoading: false,
+	loginError: false,
+	accessToken: false,
+	country: false,
+	isProfileComplete: false,
+	email: false,
+	otpCode: false,
 };
 
 export const loginUser = createAsyncThunk(
-  "login/loginUser",
-  async (formData) => {
-    const url = `${liveServer}/auth/`;
-    try {
-      const response = await axios.post(url, formData, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        withCredentials: true,
-      });
-      console.log(response.data);
-      return response.data;
-    } catch (error) {
-      sendError(error);
-    }
-  }
+	"login/loginUser",
+	async (formData) => {
+		const url = `${liveServer}/auth/`;
+		try {
+			const response = await axios.post(url, formData, {
+				headers: {
+					"Content-Type": "application/json",
+				},
+				withCredentials: true,
+			});
+			console.log(response.data);
+			return response.data;
+		} catch (error) {
+			sendError(error);
+		}
+	}
 );
 
 const loginSlice = createSlice({
-  name: "login",
-  initialState,
-  reducers: {
-    resetLogin(state) {
-      state.loginLoading = false;
-      state.loginError = false;
-      state.accessToken = false;
-      state.isProfileComplete = false;
-      state.country = false;
-      state.email = false;
-      state.otpCode = false;
-    },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(loginUser.pending, (state) => {
-        state.loginLoading = true;
-      })
-      .addCase(loginUser.fulfilled, (state, action) => {
-        state.loginLoading = false;
-        state.accessToken = action.payload.accessToken;
-        state.isProfileComplete = action.payload.isProfileComplete;
-        state.country = action.payload.country;
-        state.email = action.payload.email;
-        state.otpCode = action.payload.otpCode;
-        state.loginError = false;
-      })
-      .addCase(loginUser.rejected, (state, action) => {
-        state.loginLoading = false;
-        state.accessToken = false;
-        state.isProfileComplete = false;
-        state.country = false;
-        state.email = false;
-        state.otpCode = false;
-        state.loginError = action.error.message;
-      });
-  },
+	name: "login",
+	initialState,
+	reducers: {
+		resetLogin(state) {
+			state.loginLoading = false;
+			state.loginError = false;
+			state.accessToken = false;
+			state.isProfileComplete = false;
+			state.country = false;
+			state.email = false;
+			state.otpCode = false;
+		},
+	},
+	extraReducers: (builder) => {
+		builder
+			.addCase(loginUser.pending, (state) => {
+				state.loginLoading = true;
+			})
+			.addCase(loginUser.fulfilled, (state, action) => {
+				state.loginLoading = false;
+				state.accessToken = action.payload.accessToken;
+				state.isProfileComplete = action.payload.isProfileComplete;
+				state.country = action.payload.country;
+				state.email = action.payload.email;
+				state.otpCode = action.payload.otpCode;
+				state.loginError = false;
+			})
+			.addCase(loginUser.rejected, (state, action) => {
+				state.loginLoading = false;
+				state.accessToken = false;
+				state.isProfileComplete = false;
+				state.country = false;
+				state.email = false;
+				state.otpCode = false;
+				state.loginError = action.error.message;
+			});
+	},
 });
 
 export const { resetLogin } = loginSlice.actions;
--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -8,180 +8,180 @@ import { useDispatch, useSelector } from "react-redux";
 import { loginUser, resetLogin } from "../features/loginSlice";
 
 const Signin = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
 
-  const [form, setForm] = useState({
-    email: "",
-    password: "",
-  });
-  const [error, setError] = useState("");
+	const [form, setForm] = useState({
+		email: "",
+		password: "",
+	});
+	const [error, setError] = useState("");
 
-  const {
-    loginLoading,
-    loginError,
-    accessToken,
-    country,
-    isProfileComplete,
-    email,
-    otpCode,
-    isEmailVerified,
-  } = useSelector((state) => state.login);
+	const {
+		loginLoading,
+		loginError,
+		accessToken,
+		country,
+		isProfileComplete,
+		email,
+		otpCode,
+		isEmailVerified,
+	} = useSelector((state) => state.login);
 
-  const year = new Date().getFullYear();
+	const year = new Date().getFullYear();
 
-  const handleInput = (e) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
-  };
+	const handleInput = (e) => {
+		setForm({ ...form, [e.target.name]: e.target.value });
+	};
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+	const handleSubmit = (e) => {
+		e.preventDefault();
 
-    for (const key in form) {
-      if (form[key] === "") {
-        setError(`Please enter your ${key}`);
-        return;
-      }
-    }
+		for (const key in form) {
+			if (form[key] === "") {
+				setError(`Please enter your ${key}`);
+				return;
+			}
+		}
 
-    console.log(form);
-    dispatch(loginUser(form));
+		console.log(form);
+		dispatch(loginUser(form));
 
-    setError("");
-  };
+		setError("");
+	};
 
-  useEffect(() => {
-    if (loginError) {
-      setError(loginError);
-    }
-  }, [loginError]);
+	useEffect(() => {
+		if (loginError) {
+			setError(loginError);
+		}
+	}, [loginError]);
 
-  useEffect(() => {
-    let timeout;
-    if (error) {
-      timeout = setTimeout(() => {
-        setError("");
-        dispatch(resetLogin());
-      }, 3000);
-    }
-    return () => clearTimeout(timeout);
-  }, [error]);
+	useEffect(() => {
+		let timeout;
+		if (error) {
+			timeout = setTimeout(() => {
+				setError("");
+				dispatch(resetLogin());
+			}, 3000);
+		}
+		return () => clearTimeout(timeout);
+	}, [error]);
 
-  useEffect(() => {
-    let timeout;
-    if (accessToken) {
-      sessionStorage.setItem("accessToken", JSON.stringify(accessToken));
+	useEffect(() => {
+		let timeout;
+		if (accessToken) {
+			sessionStorage.setItem("accessToken", JSON.stringify(accessToken));
 
-      const userData = {
-        country: country,
-        isProfileComplete: isProfileComplete,
-        email: email,
-        otpCode: otpCode,
-        isEmailVerified: isEmailVerified,
-      };
+			const userData = {
+				country: country,
+				isProfileComplete: isProfileComplete,
+				email: email,
+				otpCode: otpCode,
+				isEmailVerified: isEmailVerified,
+			};
 
-      sessionStorage.setItem("userData", JSON.stringify(userData));
+			sessionStorage.setItem("userData", JSON.stringify(userData));
 
-      timeout = setTimeout(() => {
-        setForm({
-          email: "",
-          password: "",
-        });
+			timeout = setTimeout(() => {
+				setForm({
+					email: "",
+					password: "",
+				});
 
-        console.log(userData.isProfileComplete);
+				console.log(userData.isProfileComplete);
 
-        if (!userData.isProfileComplete) {
-          navigate("/personal");
-        } else {
-          navigate("/otpcode");
-        }
-      }, 3000);
-    }
-    return () => clearTimeout(timeout);
-  }, [
-    accessToken,
-    navigate,
-    country,
-    isProfileComplete,
-    email,
-    otpCode,
-    isEmailVerified,
-  ]);
+				if (!userData.isProfileComplete) {
+					navigate("/personal");
+				} else {
+					navigate("/otpcode");
+				}
+			}, 3000);
+		}
+		return () => clearTimeout(timeout);
+	}, [
+		accessToken,
+		navigate,
+		country,
+		isProfileComplete,
+		email,
+		otpCode,
+		isEmailVerified,
+	]);
 
-  useEffect(() => {
-    document.title = "Vestor - Login";
-  }, []);
-  return (
-    <div className="bg-black text-slate-100 h-screen">
-      <div className="flex flex-col md:flex-row-reverse md:items-center h-full">
-        <div className="flex flex-col gap-6 p-8 w-full md:w-[40%]">
-          <Logo customClass={"flex items-center gap-1"} />
-          <form action="" className="flex flex-col gap-4">
-            <div className={styles.formHolder}>
-              <label htmlFor="" className={styles.label}>
-                Email address <span className={styles.formSpan}>*</span>
-              </label>
-              <input
-                className={styles.input}
-                type="text"
-                placeholder="Email address"
-                name="email"
-                value={form.email}
-                onChange={handleInput}
-                autoComplete="off"
-              />
-            </div>
-            <div className={styles.formHolder}>
-              <label htmlFor="" className={styles.label}>
-                Password <span className={styles.formSpan}>*</span>
-              </label>
-              <input
-                className={styles.input}
-                type="password"
-                placeholder="Password"
-                name="password"
-                value={form.password}
-                onChange={handleInput}
-              />
-            </div>
-            <div className="flex items-center gap-1">
-              <input
-                type="checkbox"
-                // className="bg-zinc-800 outline-none border border-zinc-700"
-              />
-              <small>Remember Me</small>
-            </div>
-            <button
-              onClick={handleSubmit}
-              className="bg-green-600 text-slate-100 p-2 font-bold rounded-sm"
-            >
-              Sign In
-            </button>
-            <Link to={"/reset-pass"} className={styles.formLink}>
-              Forgot Password?
-            </Link>
-          </form>
-          <div className="flex flex-col gap-1">
-            <p>Need a vestor account?</p>
-            <Link to={"/signup"} className={styles.formLink}>
-              Sign up now!{" "}
-            </Link>
-          </div>
-          <div className="lg:mt-20">
-            <small className="text-[10px]">
-              &copy; {year} Vestor, Inc. Copyrights, logos, and trademarks are
-              property of Vestor, Inc. <br className="md:hidden" />
-              All rights reserved. Vestor, Inc., member FINRA | SIPC | NFA
-            </small>
-          </div>
-        </div>
-        <div className="w-full md:w-[70%] flex items-center h-full bgImg"></div>
-        {error && <ErrorModal error={error} />}
-        {loginLoading && <LoadingModal text={"Logging In"} />}
-        {accessToken && <Successmodal successText={"Login Successful."} />}
-      </div>
-    </div>
-  );
+	useEffect(() => {
+		document.title = "Vestor - Login";
+	}, []);
+	return (
+		<div className="bg-black text-slate-100 h-screen">
+			<div className="flex flex-col md:flex-row-reverse md:items-center h-full">
+				<div className="flex flex-col gap-6 p-8 w-full md:w-[40%]">
+					<Logo customClass={"flex items-center gap-1"} />
+					<form action="" className="flex flex-col gap-4">
+						<div className={styles.formHolder}>
+							<label htmlFor="" className={styles.label}>
+								Email address <span className={styles.formSpan}>*</span>
+							</label>
+							<input
+								className={styles.input}
+								type="text"
+								placeholder="Email address"
+								name="email"
+								value={form.email}
+								onChange={handleInput}
+								autoComplete="off"
+							/>
+						</div>
+						<div className={styles.formHolder}>
+							<label htmlFor="" className={styles.label}>
+								Password <span className={styles.formSpan}>*</span>
+							</label>
+							<input
+								className={styles.input}
+								type="password"
+								placeholder="Password"
+								name="password"
+								value={form.password}
+								onChange={handleInput}
+							/>
+						</div>
+						<div className="flex items-center gap-1">
+							<input
+								type="checkbox"
+								// className="bg-zinc-800 outline-none border border-zinc-700"
+							/>
+							<small>Remember Me</small>
+						</div>
+						<button
+							onClick={handleSubmit}
+							className="bg-green-600 text-slate-100 p-2 font-bold rounded-sm"
+						>
+							Sign In
+						</button>
+						<Link to={"/reset-pass"} className={styles.formLink}>
+							Forgot Password?
+						</Link>
+					</form>
+					<div className="flex flex-col gap-1">
+						<p>Need a vestor account?</p>
+						<Link to={"/signup"} className={styles.formLink}>
+							Sign up now!{" "}
+						</Link>
+					</div>
+					<div className="lg:mt-20">
+						<small className="text-[10px]">
+							&copy; {year} Vestor, Inc. Copyrights, logos, and trademarks are
+							property of Vestor, Inc. <br className="md:hidden" />
+							All rights reserved. Vestor, Inc., member FINRA | SIPC | NFA
+						</small>
+					</div>
+				</div>
+				<div className="w-full md:w-[70%] flex items-center h-full bgImg"></div>
+				{error && <ErrorModal error={error} />}
+				{loginLoading && <LoadingModal text={"Logging In"} />}
+				{accessToken && <Successmodal successText={"Login Successful."} />}
+			</div>
+		</div>
+	);
 };
 
 export default Signin;
